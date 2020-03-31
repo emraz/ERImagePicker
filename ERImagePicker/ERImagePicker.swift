7f5e53ba9512a3362ca634d8b2f15bb86049d9d0
Mohammad Mahmudul Hasan <mahmudul.hasan@sony.com>
2020-03-31 16:43:35 +0900

    First Commit

---
+++ ERImagePicker/ERImagePicker.swift
@@ -0,0 +1,106 @@
+//
+//  ERImagePicker.swift
+//  Demo
+//
+//  Created by Mahmudul Hasan on 2020/03/31.
+//  Copyright © 2020 Matrix Solution Ltd. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import Photos
+
+protocol ERImagePickerDelegate: class {
+    func ERImagePickerDelegate(canUseCamera accessIsAllowed: Bool, delegatedForm: ERImagePicker)
+    func ERImagePickerDelegate(canUseGallery accessIsAllowed: Bool, delegatedForm: ERImagePicker)
+    func ERImagePickerDelegate(didSelect image: UIImage, delegatedForm: ERImagePicker)
+    func ERImagePickerDelegate(didCancel delegatedForm: ERImagePicker)
+}
+
+class ERImagePicker: NSObject {
+
+    private weak var controller: UIImagePickerController?
+    weak var delegate: ERImagePickerDelegate? = nil
+
+    func present(parent viewController: UIViewController, sourceType: UIImagePickerController.SourceType) {
+        let controller = UIImagePickerController()
+        controller.delegate = self
+        controller.sourceType = sourceType
+        self.controller = controller
+        DispatchQueue.main.async {
+            viewController.present(controller, animated: true, completion: nil)
+        }
+    }
+
+    func dismiss() { controller?.dismiss(animated: true, completion: nil) }
+}
+
+extension ERImagePicker {
+
+    private func showAlert(targetName: String, completion: @escaping (Bool)->()) {
+        DispatchQueue.main.async { [weak self] in
+            guard let self = self else { return }
+            let alertVC = UIAlertController(title: "Access to the \(targetName)",
+                                            message: "Please provide access to your \(targetName)",
+                                            preferredStyle: .alert)
+            alertVC.addAction(UIAlertAction(title: "Settings", style: .default, handler: { action in
+                guard   let settingsUrl = URL(string: UIApplication.openSettingsURLString),
+                    UIApplication.shared.canOpenURL(settingsUrl) else { completion(false); return }
+                UIApplication.shared.open(settingsUrl, options: [:]) {
+                    [weak self] _ in self?.showAlert(targetName: targetName, completion: completion)
+                }
+            }))
+            alertVC.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { _ in completion(false) }))
+            UIApplication.shared.delegate?.window??.rootViewController?.present(alertVC, animated: true, completion: nil)
+        }
+    }
+
+    func cameraAsscessRequest() {
+        if AVCaptureDevice.authorizationStatus(for: .video) ==  .authorized {
+            delegate?.ERImagePickerDelegate(canUseCamera: true, delegatedForm: self)
+        } else {
+            AVCaptureDevice.requestAccess(for: .video) { [weak self] granted in
+                guard let self = self else { return }
+                if granted {
+                    self.delegate?.ERImagePickerDelegate(canUseCamera: granted, delegatedForm: self)
+                } else {
+                    self.showAlert(targetName: "camera") { self.delegate?.ERImagePickerDelegate(canUseCamera: $0, delegatedForm: self) }
+                }
+            }
+        }
+    }
+
+    func photoGalleryAsscessRequest() {
+        PHPhotoLibrary.requestAuthorization { [weak self] result in
+            guard let self = self else { return }
+            if result == .authorized {
+                DispatchQueue.main.async { [weak self] in
+                    guard let self = self else { return }
+                    self.delegate?.ERImagePickerDelegate(canUseGallery: result == .authorized, delegatedForm: self)
+                }
+            } else {
+                self.showAlert(targetName: "photo gallery") { self.delegate?.ERImagePickerDelegate(canUseCamera: $0, delegatedForm: self) }
+            }
+        }
+    }
+}
+
+extension ERImagePicker: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.editedImage] as? UIImage {
+            delegate?.ERImagePickerDelegate(didSelect: image, delegatedForm: self)
+            return
+        }
+
+        if let image = info[.originalImage] as? UIImage {
+            delegate?.ERImagePickerDelegate(didSelect: image, delegatedForm: self)
+        } else {
+            print("Other source")
+        }
+    }
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        delegate?.ERImagePickerDelegate(didCancel: self)
+    }
+}
